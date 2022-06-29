1a8980c4b4710c478493cb009a665ebad0b92841
Soumya Dey <soumyadey200@hotmail.com>
2022-06-29 15:01:51 +0530

    Add entity, repository, service, controller for Lending class

--- src/main/java/com/cognizant/controller/LendingController.java
+++ src/main/java/com/cognizant/controller/LendingController.java
@@ -1,24 +1,16 @@
 package com.cognizant.controller;
 
 import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cognizant.service.BookService;
 import com.cognizant.service.LendingService;
-import com.cognizant.service.UserService;
 import com.cognizant.utils.LendingObject;
-import com.cognizant.entity.Book;
 import com.cognizant.entity.Lending;
-import com.cognizant.entity.User;
 
 @RestController
 @RequestMapping("/lendings")
--- src/main/java/com/cognizant/controller/UserController.java
+++ src/main/java/com/cognizant/controller/UserController.java
@@ -1,18 +1,10 @@
 package com.cognizant.controller;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
